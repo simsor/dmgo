df0c0117f24393a522d17a12be2d2343eee034bb
Simon Garrelou <simon.garrelou@gmail.com>
2021-01-02 17:00:50 +0100

    Fix crash when running from KUAL(??)

--- extensions/dmgo/run.sh
+++ extensions/dmgo/run.sh
@@ -2,6 +2,6 @@
 
 /etc/init.d/framework stop
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 1
-./dmgo
+./dmgo 2>&1 > /dev/null
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 0
 /etc/init.d/framework start	
